695939acd2c269781399910c6a7c05f2fdc38722
TakesxiSximada <sximada@gmail.com>
2022-10-10 08:26:10 +0900

    Add a function `add_field` to tidb schema editor: Copy function code from django base schema editor.

--- django_tidb/schema.py
+++ django_tidb/schema.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from django.db.backends.utils import split_identifier
 from django.db.backends.mysql.schema import (
     DatabaseSchemaEditor as MysqlDatabaseSchemaEditor,
 )
@@ -34,3 +35,77 @@ class DatabaseSchemaEditor(MysqlDatabaseSchemaEditor):
 
     def _field_should_be_indexed(self, model, field):
         return False
+
+    def add_field(self, model, field):
+        """
+        Create a field on a model. Usually involves adding a column, but may
+        involve adding a table instead (for M2M fields).
+        """
+        # Special-case implicit M2M tables
+        if field.many_to_many and field.remote_field.through._meta.auto_created:
+            return self.create_model(field.remote_field.through)
+        # Get the column's definition
+        definition, params = self.column_sql(model, field, include_default=True)
+        # It might not actually have a column behind it
+        if definition is None:
+            return
+        # Check constraints can go on the column SQL here
+        db_params = field.db_parameters(connection=self.connection)
+        if db_params["check"]:
+            definition += " " + self.sql_check_constraint % db_params
+        if (
+            field.remote_field
+            and self.connection.features.supports_foreign_keys
+            and field.db_constraint
+        ):
+            constraint_suffix = "_fk_%(to_table)s_%(to_column)s"
+            # Add FK constraint inline, if supported.
+            if self.sql_create_column_inline_fk:
+                to_table = field.remote_field.model._meta.db_table
+                to_column = field.remote_field.model._meta.get_field(
+                    field.remote_field.field_name
+                ).column
+                namespace, _ = split_identifier(model._meta.db_table)
+                definition += " " + self.sql_create_column_inline_fk % {
+                    "name": self._fk_constraint_name(model, field, constraint_suffix),
+                    "namespace": "%s." % self.quote_name(namespace)
+                    if namespace
+                    else "",
+                    "column": self.quote_name(field.column),
+                    "to_table": self.quote_name(to_table),
+                    "to_column": self.quote_name(to_column),
+                    "deferrable": self.connection.ops.deferrable_sql(),
+                }
+            # Otherwise, add FK constraints later.
+            else:
+                self.deferred_sql.append(
+                    self._create_fk_sql(model, field, constraint_suffix)
+                )
+
+        # Build the SQL and run it
+        sql = self.sql_create_column % {
+            "table": self.quote_name(model._meta.db_table),
+            "column": self.quote_name(field.column),
+            "definition": definition,
+        }
+        self.execute(sql, params)
+
+        # Drop the default if we need to
+        # (Django usually does not use in-database defaults)
+        if (
+            not self.skip_default_on_alter(field)
+            and self.effective_default(field) is not None
+        ):
+            changes_sql, params = self._alter_column_default_sql(
+                model, None, field, drop=True
+            )
+            sql = self.sql_alter_column % {
+                "table": self.quote_name(model._meta.db_table),
+                "changes": changes_sql,
+            }
+            self.execute(sql, params)
+        # Add an index, if required
+        self.deferred_sql.extend(self._field_indexes_sql(model, field))
+        # Reset connection if required
+        if self.connection.features.connection_persists_old_columns:
+            self.connection.close()
